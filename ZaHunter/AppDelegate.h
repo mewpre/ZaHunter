74ed8e58a8d96e36d29f691d688573d04209e30a
mewpre <yichin.sun@gmail.com>
2015-01-21 13:30:03 -0800

    Initial Commit

---
+++ ZaHunter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZaHunter
+//
+//  Created by Yi-Chin Sun on 1/21/15.
+//  Copyright (c) 2015 Yi-Chin Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
